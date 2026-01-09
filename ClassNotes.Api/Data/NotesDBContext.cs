f44e774fe69d97b2ac3010ff4377aa12de96c26f
fromonoa <florian.hafner@htl-saalfelden.at>
2026-01-09 08:17:13 +0100

    Frontend

---
+++ ClassNotes.Api/Data/NotesDBContext.cs
@@ -0,0 +1,13 @@
+﻿using ClassNotes.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassNotes.Api.Data
+{
+    public class NotesDBContext : DbContext
+    {
+        public NotesDBContext(DbContextOptions<NotesDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Note> Notes => Set<Note>();
+    }
+}
---
+++ ClassNotes.Api/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassNotes.Api.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
